bc55a2d89b319345de1f0068ef26b615a6f15497
TobiOnabolu <onab4340@mylaurier.ca>
2023-05-06 20:01:05 -0400

    Added modification for windows

--- LongWaitScraper/start.cmd
+++ LongWaitScraper/start.cmd
@@ -1,4 +1,5 @@
 py -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel
 py -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt
 py -m proxy_scraper_checker
+py gatherProxies.py
 Pause
